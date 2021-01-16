b9d07893eb8d273aeda33f4cd66798ff5bc7202b
Ilia Urkov <exelents.s@gmail.com>
2021-01-16 16:27:20 +0300

    ololo

---
+++ create-siamese-template.sh
@@ -0,0 +1,5 @@
+export T5_MODEL="google/t5-v1_1-xxl"
+export MODEL_TEMPLATE="./templates/siamese-t5-11b-v1_1-template"
+rm -R $MODEL_TEMPLATE
+python3 -c "from siamese_model import T5Siamese;T5Siamese.init_from_base_t5_model(\"${T5_MODEL}\", \"${MODEL_TEMPLATE}\");"
+# google/t5-v1_1-xxl
